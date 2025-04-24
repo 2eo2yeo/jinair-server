9769272f3bb72d85c9a3376887d27233c92ad4e0
2eo22 <2eo2yeo@gmail.com>
2025-04-25 00:37:04 +0900

    ddd

--- repository/db.js
+++ repository/db.js
@@ -1,4 +1,6 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
     host: 'jinair-db22.cjw8casuq6rz.ap-northeast-2.rds.amazonaws.com', 
