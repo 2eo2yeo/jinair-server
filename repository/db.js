20d17514fd8df4f8b888fbe421ff87bd22690b82
2eo22 <2eo2yeo@gmail.com>
2025-04-24 16:19:43 +0900

    dd

--- repository/db.js
+++ repository/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'http://jinair-react22.s3-website.ap-northeast-2.amazonaws.com/', 
+    host: 'jinair-db22.cjw8casuq6rz.ap-northeast-2.rds.amazonaws.com', 
     port: 3306,
     user: 'admin',
     password: 'jinairadmin1234',
